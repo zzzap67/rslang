243218e0c5b4d803ed43d9ae98ac0e4b575f692e
zzzap67 <zzzap67@gmail.com>
2022-08-16 20:52:34 +0300

    init: project start

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+class App {
+  private container: HTMLElement;
+
+  constructor() {
+    this.container = document.body;
+  }
+
+  start() {
+    this.container.innerHTML = 'RSS LANG';
+  }
+}
+export default App;
