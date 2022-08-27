5e37af5c5010dcb72b4e3a0b55028f25fa36ae13
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-08-27 15:50:19 +0300

    feat: add styles to Game Start Screen

--- src/games/gameStartScreen.ts
+++ src/games/gameStartScreen.ts
@@ -1,3 +1,5 @@
+import './gameStartScreen.scss';
+
 class GameStartScreen {
   private sprintInfo = `Спринт - это тренировка на скорость.
                       Попробуй угадать как можно больше слов за 60 секунд.`;
@@ -17,21 +19,24 @@ class GameStartScreen {
 
     const mainContainer = document.body.querySelector('.main') as HTMLElement;
     mainContainer.innerHTML = `
-    <div class="game__wrapper">
-      <div class="game__info-wrapper">
-          <h2 class="game__name">${name}</h2>
-          <p class="game__info">${gameInfo}</p>
-      </div>
-      <div class="game__level-wrapper">
-          <p class="game__level-choice"></p>
-          <div class="game__btns-wrapper">
-              <button class="game__btn-level">A1</button>
-              <button class="game__btn-level">A2</button>
-              <button class="game__btn-level">B1</button>
-              <button class="game__btn-level">B2</button>
-              <button class="game__btn-level">C1</button>
-              <button class="game__btn-level">C2</button>
-          </div>
+    <div class="game__start-scr-wrapper">
+      <div class="game__image"></div>
+      <div class="game__field-wrapper">
+        <div class="game__info-wrapper">
+            <h2 class="game__name">${name}</h2>
+            <p class="game__info">${gameInfo}</p>
+        </div>
+        <div class="game__level-wrapper">
+            <p class="game__level-choice">Выбери уровень:</p>
+            <div class="game__btns-wrapper">
+                <button class="game__btn-level">A1</button>
+                <button class="game__btn-level">A2</button>
+                <button class="game__btn-level">B1</button>
+                <button class="game__btn-level">B2</button>
+                <button class="game__btn-level">C1</button>
+                <button class="game__btn-level">C2</button>
+            </div>
+        </div>
       </div>
     </div>
     `;
