81d443ccc1a7bab3535bf10e0e752783de84e951
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-09-01 17:25:30 +0300

    feat: add logout btn to header

--- src/components/main-container/main-container.ts
+++ src/components/main-container/main-container.ts
@@ -23,6 +23,7 @@ class MainContainer {
   public render() {
     //Use the next syntax for inline background images
     document.body.style.backgroundImage = 'url(' + require('../../assets/images/bg-beige.png') + ')';
+    document.body.style.backgroundSize = '100%';
     const mainContainer = document.querySelector('.main') as HTMLElement;
     mainContainer.innerHTML = '';
     this.mainHeading.innerHTML = this.html;
