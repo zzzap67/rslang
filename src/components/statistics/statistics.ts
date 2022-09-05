fe47a4c6af9bccd75b25b14f9516087be29e71ba
Gar <gar@tut.by>
2022-09-05 22:12:30 +0300

    feat: add multi stats

--- src/components/statistics/statistics.ts
+++ src/components/statistics/statistics.ts
@@ -77,25 +77,34 @@ class Statistics {
     document.body.style.backgroundImage = 'url("./img/bg-game-vertical.jpg")';
     document.body.style.backgroundSize = 'cover';
 
-    const statsInfoWrapper = new BaseElement('div', ['stats__info-wrapper']).element;
-    const statsDate = new BaseElement('h3', ['stats__h3']).element;
-    const statsHeader1 = new BaseElement('p', ['stats__p']).element;
-    const statsHeader2 = new BaseElement('p', ['stats__p']).element;
-    const statsHeader3 = new BaseElement('p', ['stats__p']).element;
-
     for (let i = 1; i <= 3; i++) {
       const stButton = document.body.querySelector('#stat-button-' + i) as HTMLElement;
       statType === i ? stButton.classList.add('stats__selected') : stButton.classList.remove('stats__selected');
+    }
+
+    const statsInfoWrapper = new BaseElement('div', ['stats__info-wrapper']).element;
+
+    if (!this.userStats.length) {
+      const statsText = new BaseElement('h3', ['stats__h3']).element;
+      const statsInfoWrapper = new BaseElement('div', ['stats__info-wrapper']).element;
+      statsText.textContent = 'Недостаточно данных для статистики. Надо заниматься.';
+      statsInfoWrapper.append(statsText);
+      statsContainer.append(statsInfoWrapper);
     }
 
     this.userStats.map((item) => {
       if (item.dates) {
         let percent = '';
         const sDate = item.dates.slice(8, 10) + '.' + item.dates.slice(5, 7) + '.' + item.dates.slice(0, 4);
+        const statsInfoContainer = new BaseElement('div', ['stats__info-container']).element;
+        const statsDate = new BaseElement('h3', ['stats__h3']).element;
+        const statsHeader1 = new BaseElement('p', ['stats__p']).element;
+        const statsHeader2 = new BaseElement('p', ['stats__p']).element;
+        const statsHeader3 = new BaseElement('p', ['stats__p']).element;
         switch (statType) {
           case 1:
             item.totalAC > 0 ? (percent = ((item.rightAC / item.totalAC) * 100).toFixed(2)) : (percent = '0.00');
-            statsDate.innerHTML = `Сегодня, <span>${sDate}</span>`;
+            statsDate.innerHTML = `<span>${sDate}</span>`;
             statsHeader1.innerHTML = `Количество новых слов: <span>${item.newWordAC}</span>`;
             statsHeader2.innerHTML = `Процент правильных ответов: <span>${percent.replace('.', ',')}</span>%`;
             statsHeader3.innerHTML = `Самая длинная серия правильных ответов: <span>${item.seriesAC}`;
@@ -104,22 +113,23 @@ class Statistics {
             item.totalSprint > 0
               ? (percent = ((item.rightSprint / item.totalSprint) * 100).toFixed(2))
               : (percent = '0.00');
-            statsDate.innerHTML = `Сегодня, <span>${sDate}</span>`;
+            statsDate.innerHTML = `<span>${sDate}</span>`;
             statsHeader1.innerHTML = `Количество новых слов: <span>${item.newWordSprint}</span>`;
-            statsHeader2.innerHTML = `Процент правильных ответов: <span>${percent}</span>%`;
+            statsHeader2.innerHTML = `Процент правильных ответов: <span>${percent.replace('.', ',')}</span>%`;
             statsHeader3.innerHTML = `Самая длинная серия правильных ответов: <span>${item.seriesSprint}</span>`;
             break;
           case 3:
             item.totalAC + item.totalSprint > 0
               ? (percent = (((item.rightAC + item.rightSprint) / (item.totalAC + item.totalSprint)) * 100).toFixed(2))
               : (percent = '0.00');
-            statsDate.innerHTML = `Сегодня, <span>${sDate}</span>`;
+            statsDate.innerHTML = `<span>${sDate}</span>`;
             statsHeader1.innerHTML = `Количество новых слов: <span>${item.newWordAC + item.newWordSprint}</span>`;
             statsHeader2.innerHTML = `Количество изученных слов: <span>${item.studiedWord}</span>`;
-            statsHeader3.innerHTML = `Процент правильных ответов: <span>${percent}</span>%`;
+            statsHeader3.innerHTML = `Процент правильных ответов: <span>${percent.replace('.', ',')}</span>%`;
             break;
         }
-        statsInfoWrapper.append(statsDate, statsHeader1, statsHeader2, statsHeader3);
+        statsInfoContainer.append(statsDate, statsHeader1, statsHeader2, statsHeader3);
+        statsInfoWrapper.append(statsInfoContainer);
         statsContainer.append(statsInfoWrapper);
       }
     });
