1eb5e15c568dfcf094d8ae962699cb152a3de626
Kseniya-Korolchuk <kkorolchuk@gmail.com>
2022-09-05 14:16:07 +0300

    feat: add stats layout

--- src/components/statistics/statistics.ts
+++ src/components/statistics/statistics.ts
@@ -16,7 +16,9 @@ class Statistics {
     this.userStats = [];
     const mainContainer = document.body.querySelector('.main') as HTMLElement;
     mainContainer.style.minHeight = '100vh';
+
     const statsWrapper = new BaseElement('div', ['stats__wrapper']).element;
+    const statsHeading = new BaseElement('h2', ['stats__h2']).element;
     const statsButtonsWrapper = new BaseElement('div', ['stats__buttons-wrapper']).element;
     const statsContainer = new BaseElement('div', ['stats__container']).element;
     const statsButtonsNames = ['Аудиовызов', 'Спринт', 'Слова'];
@@ -26,9 +28,9 @@ class Statistics {
       statsButton.addEventListener('click', (e: Event) => this.handleStatsButtons(e));
       statsButtonsWrapper.append(statsButton);
     });
-    statsWrapper.textContent = 'Stats';
+    statsHeading.textContent = 'Статистика';
     mainContainer.innerHTML = '';
-    statsWrapper.append(statsButtonsWrapper, statsContainer);
+    statsWrapper.append(statsHeading, statsButtonsWrapper, statsContainer);
 
     this.getUserStats().then(() => {
       // console.log(this.userStats);
@@ -76,6 +78,12 @@ class Statistics {
     const statsContainer = document.body.querySelector('.stats__container') as HTMLElement;
     statsContainer.innerHTML = '';
 
+    const statsInfoWrapper = new BaseElement('div', ['stats__info-wrapper']).element;
+    const statsDate = new BaseElement('h3', ['stats__h3']).element;
+    const statsHeader1 = new BaseElement('p', ['stats__p']).element;
+    const statsHeader2 = new BaseElement('p', ['stats__p']).element;
+    const statsHeader3 = new BaseElement('p', ['stats__p']).element;
+
     for (let i = 1; i <= 3; i++) {
       const stButton = document.body.querySelector('#stat-button-' + i) as HTMLElement;
       statType === i ? stButton.classList.add('stats__selected') : stButton.classList.remove('stats__selected');
@@ -83,34 +91,39 @@ class Statistics {
 
     this.userStats.map((item) => {
       if (item.dates) {
-        const statsWrapper = new BaseElement('div', ['stats__bydate']).element;
         let percent = '';
         const sDate = item.dates.slice(8, 10) + '.' + item.dates.slice(5, 7) + '.' + item.dates.slice(0, 4);
         switch (statType) {
           case 1:
             item.totalAC > 0 ? (percent = ((item.rightAC / item.totalAC) * 100).toFixed(2)) : (percent = '0.00');
-            statsWrapper.innerHTML = `${sDate} - Новых: ${item.newWordAC} | Процент: ${percent}% | Самая длинная серия: ${item.seriesAC}`;
+            statsDate.innerHTML = `Сегодня, <span>${sDate}</span>`;
+            statsHeader1.innerHTML = `Количество новых слов: <span>${item.newWordAC}</span>`;
+            statsHeader2.innerHTML = `Процент правильных ответов: <span>${percent}</span>%`;
+            statsHeader3.innerHTML = `Самая длинная серия правильных ответов: <span>${item.seriesAC}`;
             break;
           case 2:
             item.totalSprint > 0
               ? (percent = ((item.rightSprint / item.totalSprint) * 100).toFixed(2))
               : (percent = '0.00');
-            statsWrapper.innerHTML = `${sDate} - Новых: ${item.newWordSprint} | Процент: ${percent}% | Самая длинная серия: ${item.seriesSprint}`;
+            statsDate.innerHTML = `Сегодня, <span>${sDate}</span>`;
+            statsHeader1.innerHTML = `Количество новых слов: <span>${item.newWordSprint}</span>`;
+            statsHeader2.innerHTML = `Процент правильных ответов: <span>${percent}</span>%`;
+            statsHeader3.innerHTML = `Самая длинная серия правильных ответов: <span>${item.seriesSprint}</span>`;
             break;
           case 3:
             item.totalAC + item.totalSprint > 0
               ? (percent = (((item.rightAC + item.rightSprint) / (item.totalAC + item.totalSprint)) * 100).toFixed(2))
               : (percent = '0.00');
-            statsWrapper.innerHTML = `${sDate} - Новых: ${item.newWordAC + item.newWordSprint} | Изученных: ${
-              item.studiedWord
-            } | Процент: ${percent}%`;
+            statsDate.innerHTML = `Сегодня, <span>${sDate}</span>`;
+            statsHeader1.innerHTML = `Количество новых слов: <span>${item.newWordAC + item.newWordSprint}</span>`;
+            statsHeader2.innerHTML = `Количество изученных слов: <span>${item.studiedWord}</span>`;
+            statsHeader3.innerHTML = `Процент правильных ответов: <span>${percent}</span>%`;
             break;
         }
-        statsContainer.append(statsWrapper);
+        statsInfoWrapper.append(statsDate, statsHeader1, statsHeader2, statsHeader3);
+        statsContainer.append(statsInfoWrapper);
       }
     });
-
-    //statsContainer.innerHTML = 'aaaa ' + statType;
   }
 
   private handleStatsButtons(e: Event) {
